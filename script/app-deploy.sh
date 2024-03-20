24a9f03e8e1348a10af5059612aaf07caac06f77
Ho Zi Xuan <zixuanho@recogine.com>
2024-03-20 18:05:39 +0800

    chore: update script

--- script/app-deploy.sh
+++ script/app-deploy.sh
@@ -4,13 +4,13 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 WORK_DIR="${SCRIPT_DIR}/.."
 SERVICE_NAME="app"
 
+# step3. build docker
+"${WORK_DIR}"/.dev/build-docker.sh
+
 # step2. down the server
 docker-compose \
     -f "${WORK_DIR}"/"${SERVICE_NAME}".dc.yml \
     down
-
-# step3. build docker
-"${WORK_DIR}"/.dev/build-docker.sh
 
 # step4. up the server
 docker-compose \
